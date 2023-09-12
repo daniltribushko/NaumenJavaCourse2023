ba352b2d4de504e4f0216bb19ee4b2f2d63e3148
daniltribushko <danil.tribushko@bk.ru>
2023-09-12 16:40:00 +0500

    Initial commit

---
+++ src/main/java/com/example/naumen2023/Naumen2023Application.java
@@ -0,0 +1,13 @@
+package com.example.naumen2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Naumen2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Naumen2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/naumen2023/Naumen2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naumen2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Naumen2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
